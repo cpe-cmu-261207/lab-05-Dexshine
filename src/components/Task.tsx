8a853e550e961f580dc88b43d70a9bd663cfbf0d
Dexshine <b_o_y_s@hotmail.com>
2021-08-09 10:43:14 +0700

    hide done/del button when tasks is done

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -7,28 +7,38 @@ type TaskProps = {
     isDone: boolean;
 }
 
+
 const Task = ({id, name, doneFn, deleteFn, isDone} : TaskProps) => {
     //check if name has mark on it
-    const doneTask = <del>{name}</del>
-    let task = <span className="text-2xl">{doneTask}</span>
-    if(isDone === true){
-        task = <span className="text-2xl">{doneTask}</span>
+    const doneTask = <del>{name}</del> //name with line marked
+    let task = <span></span> //text template
+    let doneAppear = ''
+    let delAppear = ''
+    //check if tasks is done
+    if(isDone === true){//tasks is done
+        task = <span className="text-2xl">{doneTask}</span> //display text with line marked
+        //hide done/del button
+        doneAppear = 'bg-white invisible'
+        delAppear = 'bg-white invisible'
     }
-    else{
-        task = <span className="text-2xl">{name}</span>
+    else{//tasks isn't done
+        task = <span className="text-2xl">{name}</span> //display normal text
+        //done/del visible
+        doneAppear = 'bg-green-400 w-24 text-2xl'
+        delAppear = 'bg-red-400 w-24 text-2xl'
     }
     //remember button visibility state
     const [show,setShow] = useState(false)
     return (
         <div
             className="flex justify-between h-8 items-center py-6 border-b"
-            onMouseEnter={() => setShow(true)}//show button
-            onMouseLeave={() => setShow(false)}//hide button
+            onMouseEnter={() => setShow(true)}//show button when mouseEnter
+            onMouseLeave={() => setShow(false)}//hide button when mouseLeave
         >
             {task}
             <div className="flex space-x-1 items-center">
-            {show?<button className="bg-green-400 w-24 text-2xl" onClick={() => doneFn(id)}>Done</button>:null}
-            {show?<button className="bg-red-400 w-24 text-2xl" onClick={() => deleteFn(id)}>Delete</button>:null}
+            {show?<button className={doneAppear} onClick={() => doneFn(id)}>Done</button>:null}
+            {show?<button className={delAppear} onClick={() => deleteFn(id)}>Delete</button>:null}
             </div>
         </div>
     )
